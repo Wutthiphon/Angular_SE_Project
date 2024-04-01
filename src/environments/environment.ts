06141b759933a056d8affc18a8dce0121c8c47f1
Wutthiphon-MacbookPro <woodychgamer5588@gmail.com>
2024-04-01 21:36:52 +0700

    fix bug free course show paid

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:8080/api',
+  apiURL: "https://demos.wutthiphon.space/lab/ubuntu/serv/91/api/",
+  // apiURL: 'http://localhost:8080/api',
   // apiURL: "http://172.16.255.250:8080/api",
 };
