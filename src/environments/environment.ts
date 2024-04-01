0e5448da0ca3727d60316b1b1b1d2b4330e26389
Wutthiphon-MacbookPro <woodychgamer5588@gmail.com>
2024-04-01 21:38:17 +0700

    remove api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiURL: "https://demos.wutthiphon.space/lab/ubuntu/serv/91/api/",
-  // apiURL: 'http://localhost:8080/api',
+  apiURL: 'http://localhost:8080/api',
   // apiURL: "http://172.16.255.250:8080/api",
 };
